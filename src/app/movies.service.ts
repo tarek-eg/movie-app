d7daae44532051b775dec0b58707552050c5cbab
Tarek Mostafa <trkmos@gmail.com>
2019-08-16 07:19:03 +0200

    misc

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -1,26 +1,24 @@
-import { Injectable } from '@angular/core';
-import { Observable, of, from } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Observable, of, from } from "rxjs";
+
 import MOVIES from "./movie.mock-data.json";
-import { Movie } from './movie-details/movie-detail.model.js';
+import { Movie } from "./movie-details/movie-detail.model.js";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MoviesService {
-
-  constructor() { }
+  constructor() {}
 
   getMovies(): Observable<Movie[]> {
     return of(MOVIES);
   }
 
-  getMovie(id: number): Observable<Movie>{
-    return from(MOVIES.filter(movie => movie.id === id))
-
+  getMovie(id: number): Observable<Movie> {
+    return from(MOVIES.filter(movie => movie.id === id));
   }
 
-  search(query: string){
-      return of(MOVIES.filter(movie => movie.name.toLowerCase().includes(query)))
-
+  search(query: string) {
+    return of(MOVIES.filter(movie => movie.name.toLowerCase().includes(query)));
   }
 }
